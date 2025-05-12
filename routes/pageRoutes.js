2404644e56c4f84181356a2793b46de373a7eff3
Gideon <gideonhum402@gmail.com>
2025-05-12 14:53:39 +0200

    Enhance event display with dynamic content and styling in events page

--- routes/pageRoutes.js
+++ routes/pageRoutes.js
@@ -1,26 +1,45 @@
-const express = require('express');
+const express = require("express");
 const router = express.Router();
+const events = [
+  {
+    title: "Community Cleanup Day",
+    date: "2025-05-20",
+    location: "Greenfield Park",
+    image: "/images/cleanup.jpg",
+  },
+  {
+    title: "Charity Fun Run",
+    date: "2025-06-01",
+    location: "Central Stadium",
+    image: "/images/funrun.jpg",
+  },
+  {
+    title: "Tech Workshop",
+    date: "2025-06-10",
+    location: "Library Hall",
+    image: "/images/workshop.jpg",
+  },
+];
 
 let submissions = [];
 
-router.get('/', (req, res) => {
-    res.render('pages/home');
+router.get("/", (req, res) => {
+  res.render("pages/home");
 });
 
-router.get('/about', (req, res) => {
+router.get("/about", (req, res) => {});
+
+router.get("/events", (req, res) => {
+  res.render("pages/events", { events });
 });
 
-router.get('/events', (req, res) => {
+router.get("/contact", (req, res) => {});
+router.post("/contact", (req, res) => {
+  submissions.push(req.body);
+  res.redirect("/thankyou");
 });
-
-router.get('/contact', (req, res) => {
-});
-router.post('/contact', (req, res) =>{
-    submissions.push(req.body);
-    res.redirect('/thankyou');
-});
-router.get('/thankyou', (req, res) => {
-    res.render('pages/thankyou');
+router.get("/thankyou", (req, res) => {
+  res.render("pages/thankyou");
 });
 
 module.exports = router;
